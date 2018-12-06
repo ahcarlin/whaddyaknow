a11ed59d945f2d22233fed0af264472f65d3a5c6
AHCarl <ahcarlin@gmail.com>
2018-12-06 17:06:18 -0600

    made game edit, can edit title

--- frontend/src/editGame.js
+++ frontend/src/editGame.js
@@ -1,16 +1,22 @@
 
 const renderEditGame = function() {
-  titleBox.innerHTML = "<h2><i class='icon star is-medium'></i>Create A Game</h2>"
+  titleBox.innerHTML = `<h2><i class='icon star is-medium'></i>Edit ${selectedGame.title}</h2>`
 
   const editForm = document.createElement('form')
     editForm.innerHTML = `
         <div class='container is-rounded'>
-            <input id='title' type='text' placeholder="Title" class='input'>
-        </div>
-        <div class='container with-title'>
-            <label class='title'>How many questions?</label>
-            <input id='numOfQuestions' type='number' class='input' placeholder="number of questions">
+            <input id='title' type='text' value="${selectedGame.title}" class='input'>
         </div>
     `
+
+    editForm.append(
+      renderButton("Update", () => {
+        selectedGame.title = document.querySelector("input").value 
+        updateGame(selectedGame)
+        renderSelectedGame()
+      })
+    )
+
+    app.append(editForm)
+    return ''
 }
-renderEditGame()
