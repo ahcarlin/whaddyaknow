84a8e562f61b4bed847f69e6d794ac152713cd92
no-relation <eddiec76@gmail.com>
2018-12-05 18:25:51 -0600

    API questions almost working, but fetch is NOT HAPPENING

--- frontend/src/newGame.js
+++ frontend/src/newGame.js
@@ -22,9 +22,7 @@ const renderGameForm = () => {
         selectedGame.questions = []
         renderQuestionForm(1,selectedGame.numQuests)
     })
-    submit.setAttribute('class','btn')
     exit = renderButton('Exit', renderGamesList)
-    exit.setAttribute('class', 'btn')
 
     gameForm.append(submit, exit)
     return gameForm
@@ -49,32 +47,167 @@ const renderQuestionForm = (index, numQuests) => {
             <input class='incorrect input' type='text'>
         </div>
         `
-    submit = document.createElement('button')
-    submit.setAttribute('class', 'btn')
-    submit.setAttribute('type', 'button')
-    submit.innerHTML = 'Submit'
-    questionForm.append(submit)
-    app.append(questionForm)
-
+    // submit = document.createElement('button')
+    // submit.innerHTML = 'Submit'
+    
     let question = {}
-    submit.addEventListener('click',()=>{
-        question.content = document.getElementById('content').value
-        question.correct = document.getElementById('correct').value
-        incorrectNodes = document.querySelectorAll('.incorrect')
-        question.incorrect = []
-        incorrectNodes.forEach((node)=>question.incorrect.push(node.value))
-        selectedGame.questions.push(question)
-        if (index<numQuests){
+    if (index<=numQuests){
+        submit = renderButton('Submit',()=>{
+            question.content = document.getElementById('content').value
+            question.correct = document.getElementById('correct').value
+            incorrectNodes = document.querySelectorAll('.incorrect')
+            question.incorrect = []
+            incorrectNodes.forEach((node)=>question.incorrect.push(node.value))
+            selectedGame.questions.push(question)
             renderQuestionForm(index+1, numQuests)
+        })
+    }else{
+        saveGame(selectedGame)
+    }
+
+    doneButton = renderButton('Done',(e)=>{
+        finishQuestionList(numQuests-index)        
+    })
+    questionForm.append(submit, doneButton)
+    app.append(questionForm)
+}
+
+function finishQuestionList(num){
+    app.innerHTML = '<p>Querying the Open Trivia Database</p>'
+    apiQueryForm = document.createElement('form')
+    apiQueryForm.innerHTML = `
+        <div class='container is-rounded with-title'>
+            <label class='title'>What difficulty of questions?</label>
+            <select id='difficulty'>
+                <option value='' selected>Any</option>
+                <option value='easy'>Easy</option>
+                <option value='medium'>Medium</option>
+                <option value='hard'>Hard</option>
+            </select>
+        </div>
+        <div class='container is-rounded with-title'>
+            <label class='title'>What kind of questions?</label>
+            <select id='type'>
+                <option value=''>Any</option>
+                <option value='multiple'>Multiple Choice</option>
+                <option value='boolean'>True/False</option>
+            </select>
+        </div>
+    `
+    categorySelectorsDiv = document.createElement('div')
+    categorySelectorsDiv.setAttribute('class','container is-rounded with-title')
+    categorySelectorsDiv.inneHTML = "<label class='title'>What category?</label>"
+
+    categorySelectors = document.createElement('select')
+    categorySelectors.setAttribute('id','category')
+    categoryOptions = categoryArray.map((category)=>{
+        return `<option value='${category}'>${category}</option>`
+    })
+    categorySelectors.innerHTML = categoryOptions.join('')
+    categorySelectorsDiv.append(categorySelectors)
+    apiQueryForm.append(categorySelectorsDiv)
+    addQuestionsButton = document.createElement('button')
+    addQuestionsButton.setAttribute('id','addQuestionsButton')
+    addQuestionsButton.inneHTML = 'Get Questions'
+    apiQueryForm.append(addQuestionsButton)
+    
+    app.append(apiQueryForm)
+    addQuestionsButton.addEventListener('click',()=>{
+        queryParams = {}
+        queryParams.amount = num
+        queryParams.difficulty = document.getElementById('difficulty').value
+        queryParams.type = document.getElementById('type').value
+        queryParams.category = document.getElementById('category').value
+
+        // this should be getQuestions
+        searchParams = `?amount=${queryParams.amount}`
+
+        if (queryParams.difficulty) {
+            searchParams += `&difficulty=${queryParams.difficulty}`
         }
-        else {
-            
-            server.post('/games', selectedGame)
-            .then(function(){
-                update(function () {
-                    selectedView = 'game'
-                })
+
+        if (queryParams.type) {
+            searchParams += `&type=${queryParams.type}`
+        }
+
+        if (queryParams.category) {
+            searchParams += `&category=${categoryArray.indexOf(queryParams.category) + 9}`
+        }
+
+        let results;
+        fetch(`https://opentdb.com/api.php${searchParams}`)
+            .then((resp) => resp.json())
+            .then((data) => {
+                (results = data)
+                console.log(results)
+                // return (results.results)
             })
-        }
+            // end of getQuestions
+            console.log(results)
+            console.log(results.results)
+
+        apiQuestions = results.results.join(',')
+        selectedGame.questions.push(apiQuestions)
+        saveGame(selectedGame)
+    })    
+}
+
+function saveGame(selectedGame){
+    console.log('saving game')
+    server.post('/games', selectedGame)
+    .then(function(){
+        update(function () {
+            selectedView = 'game'
+        })
     })
 }
+
+const categoryArray = ['General Knowledge',
+    'Entertainment: Books',
+    'Entertainment: Film',
+    'Entertainment: Music',
+    'Entertainment: Musicals & Theatres',
+    'Entertainment: Television',
+    'Entertainment: Video Games',
+    'Entertainment: Board Games',
+    'Science & Nature',
+    'Science: Computers',
+    'Science: Mathematics',
+    'Mythology',
+    'Sports',
+    'Geography',
+    'History',
+    'Politics',
+    'Art',
+    'Celebrities',
+    'Animals',
+    'Vehicles',
+    'Entertainment: Comics',
+    'Science: Gadgets',
+    'Entertainment: Japanese Anime & Manga',
+    'Entertainment: Cartoon & Animations']
+
+// function getQuestions(queryParams) {
+//     searchParams = `?amount=${queryParams.amount}`
+
+//     if (queryParams.difficulty) {
+//         searchParams += `&difficulty=${queryParams.difficulty}`
+//     }
+
+//     if (queryParams.type) {
+//         searchParams += `&type=${queryParams.type}`
+//     }
+
+//     if (queryParams.category) {
+//         searchParams += `&category=${categoryArray.indexOf(queryParams.category) + 9}`
+//     }
+
+//     let results;
+//     fetch(`https://opentdb.com/api.php${searchParams}`)
+//         .then((resp) => resp.json())
+//         .then((data) => {
+//             (results = data)
+//             return (results.results)
+//         })
+//     // return results
+// }
