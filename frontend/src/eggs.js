a608c96a1eef61f04806406c84e10ef900651a08
ch0mper <hannahchang17@gmail.com>
2018-12-07 12:40:17 -0600

    updated easter egg to space bar

--- frontend/src/eggs.js
+++ frontend/src/eggs.js
@@ -1,19 +1,21 @@
-document.addEventListener('click', () => {
-  josh = document.createElement('img')
-  josh.src = 'walkright.gif'
-  josh.style.position = 'absolute'
-  josh.style.left = '0px'
-  josh.style.bottom = '100px'
-  josh.style.width = "50px"
-  document.body.append(josh)
+document.addEventListener('keydown', (e) => {
+  if(e.key == ' '){
+    josh = document.createElement('img')
+    josh.src = 'walkright.gif'
+    josh.style.position = 'absolute'
+    josh.style.left = '0px'
+    josh.style.bottom = '100px'
+    josh.style.width = "50px"
+    document.body.append(josh)
 
-  setInterval( () => {
-    let currentPosition = parseInt(josh.style.left)
-    josh.style.left = currentPosition + 6 + 'px'
-  }, 20)
+    setInterval( () => {
+      let currentPosition = parseInt(josh.style.left)
+      josh.style.left = currentPosition + 6 + 'px'
+    }, 20)
 
-  setTimeout( () => {
-    josh.remove()
-  }, 5000)
+    setTimeout( () => {
+      josh.remove()
+    }, 5000)
+  }
 
 })
